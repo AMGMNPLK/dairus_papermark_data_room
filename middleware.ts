30916a85508293640f684238273663886f3c2c51
AMGMNPLK <alvaromoraza@gmail.com>
2026-01-15 17:05:06 +0100

    fix: correctly route dairus.com as an application domain in middleware

--- middleware.ts
+++ middleware.ts
@@ -28,6 +28,7 @@ function isCustomDomain(host: string) {
         host?.includes("localhost") ||
         host?.includes("papermark.io") ||
         host?.includes("papermark.com") ||
+        host?.includes("dairus.com") ||
         host?.endsWith(".vercel.app")
       ))
   );
